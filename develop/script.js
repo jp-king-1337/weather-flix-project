c3e47f1accdc42bde8c5d7d10c5e12e152f790bf
JT <raikiri24@gmail.com>
2023-06-14 14:20:10 -0400

    added api

--- develop/script.js
+++ develop/script.js
@@ -12,5 +12,10 @@ movieKey = '7355d3ba';
 
 
 
+var baseURL = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weatherKey}';
+var weatherKey = '96515e32da8200e2651c60008ed403ea';
+var url = baseURL.replace('{weatherKey}', weatherKey);
+console.log(url);
 
 
+navigator.geolocation.getCurrentPosition(showPosition)
