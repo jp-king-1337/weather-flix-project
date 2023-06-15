47f4a390de7829848fbe98103dab9d8b11a1c97b
Jamil Barrett <jamilbarrett@jamils-imac.myfiosgateway.com>
2023-06-15 11:27:16 -0400

    fixed weather

--- develop/script.js
+++ develop/script.js
@@ -1,7 +1,7 @@
 // have fun with coding this:)
 movieKey = "7355d3ba";
 
-var movieNameRef =  $('movie-name');
+var movieNameRef = $('movie-name');
 var searchBTn = $('search-btn');
 var result = $('result');
 
@@ -9,43 +9,39 @@ var result = $('result');
 
 
 $(document).ready(function () {
-  $(".sidenav").sidenav();
+    $(".sidenav").sidenav();
 
-  $("#enter-home-btn").click(function () {
-    $(".initial-container").addClass("display-none");
-    $("#home").removeClass("display-none");
-  });
+    $("#enter-home-btn").click(function () {
+        $(".initial-container").addClass("display-none");
+        $("#home").removeClass("display-none");
+    });
 });
 
+// var x = document.getElementById("demo");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        $('body').innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
 
-
-
-
-var baseURL = "https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={lon}&appid={weatherKey}";
-var weatherKey = "96515e32da8200e2651c60008ed403ea";
-var url = baseURL;
-baseURL.replace('{weatherKey}', weatherKey);
-baseURL.replace('{latitude}', latitude);
-baseURL.replace('{longitude}', longitude);
-
-
-
-getCurrentPosition()
-  .then(function (position) {
+function showPosition(position) {
+    console.log(position)
+    $('body').innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
 
     var weatherKey = "96515e32da8200e2651c60008ed403ea";
-    var baseURL = "https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={weatherKey}";
-    var url = baseURL
-      .replace("{latitude}", latitude)
-      .replace("{longitude}", longitude)
-      .replace("{weatherKey}", weatherKey);
+    var baseURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weatherKey}`;
+    console.log("API URL:", baseURL);
+    fetch(baseURL).then(function(response) {
+        return response.json()
+    }).then(function(res) {
+        console.log(res)
+    })
+}
+getLocation();
 
-    console.log("API URL:", url);
 
-    // Perform further actions with the URL, such as making an API request
-  })
-  .catch(function (error) {
-    console.log("Error:", error);
-  });
