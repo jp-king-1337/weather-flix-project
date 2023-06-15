ce73837bb490f2eae6bb4cc8bb3f2f2e4928e7ce
Thomas Szentirmay <tlszent@outlook.com>
2023-06-15 11:29:02 -0400

    logic for weather icons

--- develop/script.js
+++ develop/script.js
@@ -60,12 +60,30 @@ getCurrentPosition()
         $("#temperature").text("Temperature: " + temperature + " K");
         $("#humidity").text("Humidity: " + humidity + "%");
 
+        // This is what the logic will look like to display weather icons based on weather
+        if (weatherDescription.includes("cloudy")) {
+          var imageURL = './assets/cloudy-icon.png';
+          $("#myImage").attr("src", imageURL);
+        } if (weatherDescription.includes("raining")) {
+          var imageURL = './assets/rain-icon.png';
+          $("#myImage").attr("src", imageURL);
+        } if(weatherDescription.includes("lightning")) {
+          var imageURL = './assets/lightning-icon.png';
+          $("#myImage").attr("src", imageURL);
+        } if(weatherDescription.includes("snow")) {
+          var imageURL = './assets/snowing-icon.png';
+          $("#myImage").attr("src", imageURL);
+        } if(weatherDescription.includes("sun")) {
+          var imageURL = './assets/sunny-icon.png';
+          $("#myImage").attr("src", imageURL);
+        }; 
+
       },
       error: function (error) {
         console.log("Error:", error);
       },
     });
   })
-  .catch (function (error) {
-  console.log("Error:", error);
-});
+  .catch(function (error) {
+    console.log("Error:", error);
+  });
